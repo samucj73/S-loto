54d6f38da2803863f5694db326ff1dfea4bab713
samucj73 <samu.rcj@gmail.com>
2025-05-16 22:21:17 -0300

    Update api_lotofacil.py

--- api_lotofacil.py
+++ api_lotofacil.py
@@ -15,7 +15,7 @@ def capturar_ultimos_resultados(qtd=10):
         numero_atual = int(ultimo.get("concurso"))
         concursos.append((numero_atual, [int(d) for d in ultimo.get("dezenas")]))
 
-        # Agora busca os anteriores
+        # Agora busca os concursos anteriores
         for i in range(1, qtd):
             resp = requests.get(url_base + str(numero_atual - i))
             if resp.status_code == 200:
