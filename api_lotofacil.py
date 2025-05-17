4b4f0d1fb33795b845a320e72dcdf4201f2255cf
samucj73 <samu.rcj@gmail.com>
2025-05-16 22:11:50 -0300

    Update api_lotofacil.py

--- api_lotofacil.py
+++ api_lotofacil.py
@@ -1,17 +1,26 @@
 import requests
 
 def capturar_ultimos_resultados(qtd=10):
-    url = "https://loteriascaixa-api.herokuapp.com/api/lotofacil/"
+    url_base = "https://loteriascaixa-api.herokuapp.com/api/lotofacil/"
     concursos = []
 
     try:
-        for i in range(qtd):
-            resp = requests.get(url + str(-i))  # concursos mais recentes
+        # Primeiro busca o último concurso
+        resp = requests.get(url_base)
+        if resp.status_code != 200:
+            print("Erro ao buscar o último concurso")
+            return []
+
+        ultimo = resp.json()
+        numero_atual = int(ultimo.get("concurso"))
+        concursos.append((numero_atual, [int(d) for d in ultimo.get("dezenas")]))
+
+        # Agora busca os anteriores
+        for i in range(1, qtd):
+            resp = requests.get(url_base + str(numero_atual - i))
             if resp.status_code == 200:
                 data = resp.json()
-                numero = data.get("concurso")
-                dezenas = [int(d) for d in data.get("dezenas")]
-                concursos.append((numero, dezenas))
+                concursos.append((data.get("concurso"), [int(d) for d in data.get("dezenas")]))
             else:
                 break
     except Exception as e:
